03f224b8748e84af4646a73417bf4552b1371058
guswowh <guswowh88@gmail.com>
2022-12-12 19:23:44 +0900

    🔧  넷리파이 프록시 설정 스크립트 추가

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   server: {
     // 해석
     // - /api 앞 부분인 origin을 target에 설정되어 있는 부분으로 바꿔준다는 것을 의미한다. (정규표현식을 통해서)
+    open: true,
     proxy: {
       '/api': {
         target: 'https://apis.data.go.kr/',
